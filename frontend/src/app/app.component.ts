5a92eff0abb2c0230d0677ee01cffa86ee6adb7f
Markus Kollers <m.kollers@vertical.de>
2020-05-30 22:28:32 +0200

    Improve main app selector

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'neward-root',
+  selector: '[neward-root]',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-}
+export class AppComponent { }
