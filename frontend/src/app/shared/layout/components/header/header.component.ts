1364d048b185503edc212e89a05b5e8d35ddd4c9
Markus Kollers <m.kollers@vertical.de>
2020-05-30 23:06:45 +0200

    Scroll shadow transitions

--- frontend/src/app/shared/layout/components/header/header.component.ts
+++ frontend/src/app/shared/layout/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, HostListener } from '@angular/core';
 import { HeaderService } from '@shared/layout/services/header.service';
 import { Observable } from 'rxjs';
 
@@ -12,9 +12,19 @@ import { Observable } from 'rxjs';
   }
 })
 export class HeaderComponent {
+  @HostBinding('class.small') isSmall = false;
   navigateBackUri$: Observable<string | any[]>;
 
   constructor(headerService: HeaderService) {
     this.navigateBackUri$ = headerService.navigateBackUri$;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if (window.pageYOffset > 64) {
+      this.isSmall = true;
+    } else {
+      this.isSmall = false;
+    }
   }
 }
