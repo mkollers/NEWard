b1ef0678654bbc696babfb457b6c6523d6c59eda
Markus Kollers <m.kollers@vertical.de>
2020-06-01 00:41:38 +0200

    Add firebase

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+import { environment as dev } from './environment';
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: dev.firebase
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBOLQIiDBo1RicZWZKJUaXLr_lO_dK7nWA',
+    authDomain: 'bodylife-neward.firebaseapp.com',
+    databaseURL: 'https://bodylife-neward.firebaseio.com',
+    projectId: 'bodylife-neward',
+    storageBucket: 'bodylife-neward.appspot.com',
+    messagingSenderId: '1094727823839',
+    appId: '1:1094727823839:web:0201cd5052782f025d546a'
+  }
 };
 
 /*
