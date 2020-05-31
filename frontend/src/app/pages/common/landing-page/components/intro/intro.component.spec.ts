24b9a2279ede646281bad13174079aef82f8b51a
Markus Kollers <m.kollers@vertical.de>
2020-05-31 21:52:53 +0200

    Move to own component and improve layout

---
+++ frontend/src/app/pages/common/landing-page/components/intro/intro.component.spec.ts
@@ -0,0 +1,11 @@
+import { IntroComponent } from './intro.component';
+
+describe('IntroComponent', () => {
+  it('should create', () => {
+    // Act
+    const component = new IntroComponent();
+
+    // Assert
+    expect(component).toBeTruthy();
+  });
+});
--- frontend/src/app/pages/common/landing-page/landing-page.module.ts
+++ frontend/src/app/pages/common/landing-page/landing-page.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 
+import { IntroComponent } from './components/intro/intro.component';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
 
 @NgModule({
-  declarations: [LandingPageComponent],
+  declarations: [LandingPageComponent, IntroComponent],
   imports: [
     CommonModule,
     LandingPageRoutingModule,
