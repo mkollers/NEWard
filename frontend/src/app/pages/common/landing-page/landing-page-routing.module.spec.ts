4d0cd4965a5d2e9f5a725513de762f68e39d933a
Markus Kollers <m.kollers@vertical.de>
2020-05-31 16:25:08 +0200

    Init landing page module

---
+++ frontend/src/app/pages/common/landing-page/landing-page-routing.module.spec.ts
@@ -0,0 +1,11 @@
+import { LandingPageRoutingModule } from './landing-page-routing.module';
+
+describe('LandingPageRoutingModule', () => {
+    it('should create', () => {
+        // Act
+        const module = new LandingPageRoutingModule();
+
+        // Assert
+        expect(module).toBeTruthy();
+    });
+});
---
+++ frontend/src/app/pages/common/landing-page/landing-page.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { LandingPageRoutingModule } from './landing-page-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LandingPageRoutingModule
+  ]
+})
+export class LandingPageModule { }
