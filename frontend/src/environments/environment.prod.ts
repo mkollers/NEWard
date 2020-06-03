99643e480c5177116a1f6be9d57d93b7b3c48e43
Markus Kollers <m.kollers@vertical.de>
2020-06-04 01:00:56 +0200

    Copy config instead of referencing

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
-import { environment as dev } from './environment';
-
 export const environment: any = {
   production: true,
-  firebase: dev.firebase
+  firebase: {
+    apiKey: 'AIzaSyBOLQIiDBo1RicZWZKJUaXLr_lO_dK7nWA',
+    authDomain: 'bodylife-neward.firebaseapp.com',
+    databaseURL: 'https://bodylife-neward.firebaseio.com',
+    projectId: 'bodylife-neward',
+    storageBucket: 'bodylife-neward.appspot.com',
+    messagingSenderId: '1094727823839',
+    appId: '1:1094727823839:web:0201cd5052782f025d546a'
+  }
 };
