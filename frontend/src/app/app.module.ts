255bd76c0fe98ce140df8230300c8f57029b0fe2
Markus Kollers <m.kollers@vertical.de>
2020-06-11 21:20:11 +0200

    Fix date format

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,6 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,6 +10,8 @@ import { LayoutModule } from '@shared/layout/layout.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+registerLocaleData(localeDe, 'de');
 
 @NgModule({
   declarations: [
@@ -25,7 +29,9 @@ import { AppComponent } from './app.component';
     // Custom
     LayoutModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'de-DE' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
