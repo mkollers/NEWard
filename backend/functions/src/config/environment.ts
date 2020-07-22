686e7a7a6462fd7f8e07fb006dfa03bded2f9b43
Markus Kollers <m.kollers@vertical.de>
2020-07-23 00:39:14 +0200

    Fix compile errors

--- backend/functions/src/config/environment.ts
+++ backend/functions/src/config/environment.ts
@@ -5,8 +5,8 @@ export const environment = {
         apiKey: functions.config().mail.api_key,
         apiSecret: functions.config().mail.api_secret,
         from: {
-            email: functions.config().mail.from.email || 'bodylife@markuskollers.de',
-            name: functions.config().mail.from.name || 'NEWard'
+            email: functions.config().mail.from?.email || 'bodylife@markuskollers.de',
+            name: functions.config().mail.from?.name || 'NEWard'
         },
         templates: {
             signin: functions.config().mail.templates.signin
