14b200f64b198cd8b1530192c221c6ea462df7df
Markus Kollers <m.kollers@vertical.de>
2020-07-23 17:17:37 +0200

    Fix tests

--- frontend/src/app/pages/auth/signin-callback-page/signin-callback-page.component.spec.ts
+++ frontend/src/app/pages/auth/signin-callback-page/signin-callback-page.component.spec.ts
@@ -1,25 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async } from '@angular/core/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '@shared/auth/services/auth.service';
+import { instance, mock } from 'ts-mockito';
 
 import { SigninCallbackPageComponent } from './signin-callback-page.component';
 
 describe('SigninCallbackPageComponent', () => {
-  let component: SigninCallbackPageComponent;
-  let fixture: ComponentFixture<SigninCallbackPageComponent>;
+  let authServiceMock: AuthService;
+  let routeMock: ActivatedRoute;
+  let routerMock: Router;
+  let snackbarMock: MatSnackBar;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SigninCallbackPageComponent ]
-    })
-    .compileComponents();
+    authServiceMock = mock<AuthService>();
+    routeMock = mock<ActivatedRoute>();
+    routerMock = mock<Router>();
+    snackbarMock = mock<MatSnackBar>();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SigninCallbackPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
   it('should create', () => {
+    // Arrange
+    const authService = instance(authServiceMock);
+    const route = instance(routeMock);
+    const router = instance(routerMock);
+    const snackbar = instance(snackbarMock);
+
+    // Act
+    const component = new SigninCallbackPageComponent(authService, route, router, snackbar);
+
+    // Assert
     expect(component).toBeTruthy();
   });
 });
