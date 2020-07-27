972352c378a2229d5e3cdb041def54be44948272
Markus Kollers <m.kollers@vertical.de>
2020-07-27 02:15:29 +0200

    Improve analytics events

--- frontend/src/app/pages/auth/signin-callback-page/signin-callback-page.component.ts
+++ frontend/src/app/pages/auth/signin-callback-page/signin-callback-page.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, Injector } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Token } from '@shared/auth/models/token';
 import { AuthService } from '@shared/auth/services/auth.service';
+import { AnalyticsService } from '@shared/tracing/services/analytics.service';
 import { first } from 'rxjs/operators';
 
 @Component({
@@ -14,62 +15,78 @@ import { first } from 'rxjs/operators';
 export class SigninCallbackPageComponent {
 
   constructor(
-    private _authService: AuthService,
-    private _route: ActivatedRoute,
-    private _router: Router,
-    private _snackbar: MatSnackBar
+    @Inject(Injector) private _injector: Injector
   ) {
     this._handleAccessToken();
   }
 
   private async _handleAccessToken() {
-    const token = this._parseToken(this._route.snapshot.queryParamMap);
+    const analyticsService = this._injector.get(AnalyticsService);
+    const authService = this._injector.get(AuthService);
+    const router = this._injector.get(Router);
+    const snackbar = this._injector.get(MatSnackBar);
+
+    const token = this._parseToken();
     const url = localStorage.getItem('redirect-uri') || '/';
 
     if (!token) {
-      this._snackbar.open('Es konnte kein gültiger Token gefunden werden. Bitte lassen Sie sich eine neue E-Mail zuschicken.', '',
+      snackbar.open('Es konnte kein gültiger Token gefunden werden. Bitte lassen Sie sich eine neue E-Mail zuschicken.', '',
         { duration: 20000 }
       );
-      (window as any).ga('send', 'event', 'no_token_found');
-      this._router.navigateByUrl(url);
+      analyticsService.gtag('event', 'open_signin_mail', {
+        event_category: 'login',
+        event_label: 'no_token_found'
+      });
+      router.navigateByUrl(url);
     }
 
     let data: Token | undefined;
     try {
-      data = await this._authService.getByToken(token as string).pipe(
-        first()
-      ).toPromise();
+      data = await authService.getByToken(token).pipe(first()).toPromise();
     } catch (err) {
       let msg = 'Es konnte kein gültiger Token gefunden werden. Wenn Sie mehrfach den Zugriff angefordert haben, verwenden Sie bitte den Link aus der neusten E-Mail. Sollte es weiterhin zu Problemen kommen lassen Sie sich eine neue E-Mail zuschicken.';
       switch (err.code) {
         case 'permission-denied':
           msg = 'Ihr Link ist leider abgelaufen. Bitte lassen Sie sich eine neue E-Mail zuschicken.';
-          (window as any).ga('send', 'event', 'token_expired');
+          analyticsService.gtag('event', 'open_signin_mail', {
+            event_category: 'login',
+            event_label: 'token_expired'
+          });
           break;
         default:
-          (window as any).ga('send', 'event', 'no_token_found');
+          analyticsService.gtag('event', 'open_signin_mail', {
+            event_category: 'login',
+            event_label: 'no_token_found'
+          });
           break;
       }
-      this._snackbar.open(msg, '', { duration: 20000 });
-      this._router.navigateByUrl(url);
+      snackbar.open(msg, '', { duration: 20000 });
+      router.navigateByUrl(url);
     }
 
     if (!data) {
-      this._snackbar.open('Es konnte kein gültiger Token gefunden werden. Bitte lassen Sie sich eine neue E-Mail zuschicken.', '', {
+      snackbar.open('Es konnte kein gültiger Token gefunden werden. Bitte lassen Sie sich eine neue E-Mail zuschicken.', '', {
         duration: 20000
       });
-      (window as any).ga('send', 'event', 'no_token_found');
-      this._router.navigateByUrl(url);
+      analyticsService.gtag('event', 'open_signin_mail', {
+        event_category: 'login',
+        event_label: 'no_token_found'
+      });
+      router.navigateByUrl(url);
     }
 
-    this._authService.token$ = this._authService.getByToken(token as string);
-    this._router.navigateByUrl(url);
+    authService.token$ = authService.getByToken(token as string);
+    router.navigateByUrl(url);
   }
 
-  private _parseToken(params: ParamMap): string | null {
-    if (params.has('token')) {
-      localStorage.setItem('token', params.get('token') as string);
+  private _parseToken(): string {
+    const route = this._injector.get(ActivatedRoute);
+
+    let token = '';
+    if (route.snapshot.queryParamMap.has('token')) {
+      token = route.snapshot.queryParamMap.get('token') as string;
+      localStorage.setItem('token', token);
     }
-    return localStorage.getItem('token');
+    return token;
   }
 }
