cef7f0a4d3c947198ff9ff80be5b9b37dda2ae38
Markus Kollers <m.kollers@vertical.de>
2020-09-06 14:39:48 +0200

    Fix linting error

--- frontend/src/app/pages/admin/signin-page/signin-page.component.spec.ts
+++ frontend/src/app/pages/admin/signin-page/signin-page.component.spec.ts
@@ -1,3 +1,9 @@
 describe('SigninPageComponent', () => {
+  // // Arrange
 
+  // // Act
+  // const component = new SigninPageComponent();
+
+  // // Assert
+  // expect(component).toBeTruthy();
 });
