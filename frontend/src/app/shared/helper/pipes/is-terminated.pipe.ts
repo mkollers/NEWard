bdbd5739af3782fafc5b01b45176016d57131b82
Markus Kollers <mail@markuskollers.de>
2021-07-31 23:46:49 +0200

    Fix dates

--- frontend/src/app/shared/helper/pipes/is-terminated.pipe.ts
+++ frontend/src/app/shared/helper/pipes/is-terminated.pipe.ts
@@ -7,8 +7,8 @@ export class IsTerminatedPipe implements PipeTransform {
 
   transform(condition: 'before' | 'during' | 'after'): boolean {
     const today = new Date();
-    const start = new Date('2021-08-01T23:00:00.000Z');
-    const end = new Date('2021-10-14T22:59:59.000Z');
+    const start = new Date('2021-08-01T04:00:00.000Z');
+    const end = new Date('2021-10-14T21:59:59.000Z');
 
     const started = today > start;
     const ended = today > end;
