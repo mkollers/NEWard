424ac940c90063d26edc58924983766097a07a83
Markus Kollers <mail@markuskollers.de>
2022-08-29 09:45:25 +0200

    Update to 2022

--- frontend/src/app/shared/helper/pipes/is-terminated.pipe.ts
+++ frontend/src/app/shared/helper/pipes/is-terminated.pipe.ts
@@ -7,8 +7,8 @@ export class IsTerminatedPipe implements PipeTransform {
 
   transform(condition: 'before' | 'during' | 'after'): boolean {
     const today = new Date();
-    const start = new Date('2021-08-01T04:00:00.000Z');
-    const end = new Date('2021-10-14T21:59:59.000Z');
+    const start = new Date('2022-09-01T00:00:00.000Z');
+    const end = new Date('2022-10-30T21:59:59.000Z');
 
     const started = today > start;
     const ended = today > end;
